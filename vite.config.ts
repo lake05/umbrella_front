d696a90d594f27f423145eef195d907a0c4369ee
dou <doudou0556@gmail.com>
2022-12-20 01:27:06 +0800

    feature/add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
-
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: "happy-dom",
+  },
   plugins: [vue(), vueJsx(), Components({})],
 });
